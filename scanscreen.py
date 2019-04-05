25e213b21dd949cbef74e3dd7eea01a964afbe5d
bhavyaarora <arora13bhavya@gmail.com>
2019-04-05 23:27:32 +0530

    Add files via upload

---
+++ scanscreen.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+
+#Used headers/agent as the request timed out and asking for agent. Using following code you can fake the agent.
+headers={'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'}
+
+url=[]
+for i in range(1,9):
+    response = requests.get("https://www.swiggy.com/chennai/omr-perungudi-restaurants?page="+str(i),headers=headers)
+    content = response.content
+    soup = BeautifulSoup(content,"html.parser")
+    data = json.loads(soup.find('script', type='application/ld+json').text  )
+    dic=data['itemListElement']
+    for i in range(len(dic)):
+        a=dic[i]['url']
+        url.append(a)
+
+
+longitude=[]
+latitude=[]
+price=[]
+for i in range(len(url)):
+    response=requests.get(url[i])
+    content=response.content
+    soup=BeautifulSoup(content,"html.parser")
+    data=json.loads(soup.find('script', type='application/ld+json').text)
+    lon=data['geo']['longitude']
+    lat=data['geo']['latitude']
+    pri=data['priceRange']
+    longitude.append(lon)
+    latitude.append(lat)
+    price.append(pri)
+
+
+swiggy=pd.DataFrame() 
+
+
+swiggy['longitude']=longitude
+swiggy['latitude']=latitude
+swiggy['price']=price
+
+swiggy.to_csv("scrape_new.csv",index=False)
+
